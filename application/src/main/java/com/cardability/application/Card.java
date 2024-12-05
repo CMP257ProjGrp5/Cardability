7b6fa5cbcfe8ca773425eea37d27c4f0bb506f62
makinlemonade <lemonadegrenade@protonmail.com>
2024-12-05 12:20:43 +0400

    minor changes

--- application/src/main/java/com/cardability/application/Card.java
+++ application/src/main/java/com/cardability/application/Card.java
@@ -1,11 +1,8 @@
 package com.cardability.application;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-
+@Table(schema = "Card")
 @Entity
 public class Card {
 
--- application/src/main/java/com/cardability/application/User.java
+++ application/src/main/java/com/cardability/application/User.java
@@ -1,4 +1,16 @@
 package com.cardability.application;
 
+import jakarta.persistence.*;
+
+@Table(schema = "User")
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    String username;
+
+    String password;
 }
